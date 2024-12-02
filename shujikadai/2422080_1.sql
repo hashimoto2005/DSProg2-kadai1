3d6842ef667435fb4438e7517035fceff9f18dc6
hashimoto2005 <s2422080@stu.musashino-u.ac.jp>
2024-12-02 23:04:29 +0900

    gitignore を追加

--- shujikadai/2422080_1.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    * 
-FROM 
-    minato_restaurant 
-WHERE 
-    block = '南麻布四丁目' 
-AND 
-    business_type = '飲食店営業';
--- shujikadai/2422080_2.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    block,
-    COUNT(*) AS other_business_count
-FROM 
-    minato_restaurant
-WHERE 
-    business_type != '飲食店営業'
-GROUP BY 
-    block;
